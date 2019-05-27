551bca76672b8092b842a7e67a4ec473ab28bd26
Sarat <sarat.mahavratayajula@sherwin.com>
2019-05-27 10:50:38 -0400

    bunnyears1

---
+++ CodingBat_BunnyEars1.java
@@ -0,0 +1,19 @@
+public class CodingBat_BunnyEars1 {
+
+    public static void main(String[] args) {
+        int bunnys = 32;
+        int num_ears = bunnyEars1(bunnys);
+
+        System.out.println("Number of ears --> " + num_ears);
+    }
+
+    public static int bunnyEars1(int bunnys) {
+        if(bunnys == 1) {
+            return 2;
+        }
+        else {
+            return bunnyEars1(bunnys - 1) + 2;
+        }
+    } 
+    
+}
