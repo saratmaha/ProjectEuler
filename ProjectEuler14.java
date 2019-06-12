ba5f482344a23b234e6b7cc3962b3779a7815bed
Sarat <sarat.mahavratayajula@sherwin.com>
2019-06-11 21:26:33 -0400

    fix datatype issue

--- ProjectEuler14.java
+++ ProjectEuler14.java
@@ -5,7 +5,7 @@
  *          store the result for every number upto n 
  * Step3: Retrive the result from the stored values
  */
-import java.time.Instant;
+
 import java.util.*;
 public class ProjectEuler14 {
     public static void main(String[] args){
@@ -13,19 +13,15 @@ public class ProjectEuler14 {
         Map<Integer,Long> resultMap = new HashMap<>();       
 
         for(int i = 1; i <= 1000000  ; i++) {
-            if(i == 100000 || i == 200000 || i == 300000 || i == 400000 || i == 500000|| i == 600000 || i == 700000
-                || i == 800000 || i == 900000 || i == 999999) {
-                    System.out.println( Instant.now() );
-            }
             long count = 1;
-            int j = i;
+            long j = i;
             while( j != 1 ) {
 
                 // Use already preserved values in map to reduce re-computing
-                if(resultMap.containsKey(j)) {
+               if(resultMap.containsKey(j)) {
                     count = count + (resultMap.get(j) - 1);
                     break;
-                }
+                } 
                 if( j % 2 == 0) {
                     count = count + 1;
                     j = j / 2;                    
